eef753c80e3c4af2a879d6a02071d556be1a8c1f
Lorenzo <lorenzzo.acquesta@gmail.com>
2024-06-16 17:00:00 -0300

    atualizando rota

--- scripts/login.js
+++ scripts/login.js
@@ -39,7 +39,7 @@ button.addEventListener('click', () => {
         if(usuario){
             alert(`Bem-vindo ${usuario.nome}!`)
             
-            window.location.href= "../pages/inicio.html"
+            window.location.href= "/aroundcity/pages/inicio.html"
         }else{
             alert('Email ou senha incorretos')
         }
